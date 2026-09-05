f331d4fc641ce4ae52ea66e379eec82a6054e166
Prachit21 <prachitbarot@gmail.com>
2026-09-05 21:05:42 +0530

    Week 9: MongoDB and Mongoose

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,21 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
 const app = express();
 
 app.use(express.json());
 
-app.get('/api/users', (req, res) => {
-  res.json([{ id: 1, name: 'Prachit' }]);
+mongoose.connect('mongodb://127.0.0.1:27017/fsd_db')
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.error(err));
+
+const productSchema = new mongoose.Schema({
+  title: String,
+  price: Number
 });
 
-app.post('/api/users', (req, res) => {
-  const newUser = req.body;
-  res.status(201).json({ message: 'User added', user: newUser });
-});
+const Product = mongoose.model('Product', productSchema);
 
-app.listen(5000, () => console.log('Express running on 5000'));
+app.listen(5000, () => {
+  console.log('Express running on 5000');
+});
