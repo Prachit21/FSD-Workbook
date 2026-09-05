4cfcaab806e194005ba82ca65b6d0a2ef63e1bdd
Prachit21 <prachitbarot@gmail.com>
2026-09-05 20:17:07 +0530

    Week 8: Express.js and Routing

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,15 @@
-// server.js (Node Core HTTP Server)
-const http = require('http');
+const express = require('express');
+const app = express();
 
-const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'application/json' });
-  res.end(JSON.stringify({ message: 'Hello from Node.js Backend!' }));
+app.use(express.json());
+
+app.get('/api/users', (req, res) => {
+  res.json([{ id: 1, name: 'Prachit' }]);
 });
 
-server.listen(5000, () => {
-  console.log('Server running on port 5000');
+app.post('/api/users', (req, res) => {
+  const newUser = req.body;
+  res.status(201).json({ message: 'User added', user: newUser });
 });
+
+app.listen(5000, () => console.log('Express running on 5000'));
